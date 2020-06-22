a321d790a9ca79ac1f250151316c8a09086adaec
Your global username <your@email.com>
2020-06-22 17:16:09 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormDataService } from './services/form-data.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'multistageform';
+  constructor(public formDataService : FormDataService) {
+  }
 }
+
+
